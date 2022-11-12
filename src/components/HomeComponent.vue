fb6da891dcd4a0e6a0adbd88fc5a64ab2d1d6c9e
Osman Barrera <Osmanbj2001@gmail.com>
2022-11-12 13:41:38 -0700

    Cambio

---
+++ src/components/HomeComponent.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="hello">
+    <router-link class="dropdown-item" :to="{name: 'ChatView'}">Favoritos</router-link>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HomePage",
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+
+
