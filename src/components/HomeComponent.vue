c5ffcffedac9a785bd7617be798cf7940e2dee59
Luis Daniel Aldana de la Rosa <ldaldana001@gmail.com>
2022-11-12 19:55:06 -0600

    Added header menu

--- src/components/HomeComponent.vue
+++ src/components/HomeComponent.vue
@@ -1,8 +1,28 @@
 <template>
-  <div class="hello">
-    <router-link class="dropdown-item" :to="{name: 'ChatView'}">Favoritos</router-link>
-    <router-view></router-view>
-  </div>
+
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+      <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+        <li class="nav-item">
+          <img src="../assets/lendsome.png" width="150" height="30" class="d-inline-block align-top" alt="">
+        </li>
+        <li class="nav-item">
+          <a class="nav-link">Inicio</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link">Mis Prestamos</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link">En Posecion</a>
+        </li>
+        <li class="nav-item">
+          <router-link class="dropdown-item" :to="{name: 'ChatView'}">Favoritos</router-link>
+        </li>
+      </ul>
+    </div>
+  </nav>
+  
+
 </template>
 
 <script>
